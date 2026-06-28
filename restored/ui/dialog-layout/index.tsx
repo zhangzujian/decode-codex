6460f62f53cdd9026ea0394f6666482c10366a42
Jim Liu 宝玉 <junminliu@gmail.com>
2026-06-27 19:40:52 -0500

    ui: restore local thread support components

---
+++ restored/ui/dialog-layout/index.tsx
@@ -0,0 +1,39 @@
+// Restored from ref/webview/assets/dialog-layout-BUsOXjxz.js
+// Public dialog layout exports.
+
+import { DialogLayout } from "./dialog-layout-content";
+import {
+  DIALOG_FOOTER_BUTTON_CLASS,
+  DIALOG_OVERLAY_CLASS_NAME,
+  DialogContent,
+  DialogDescription,
+  DialogOverlay,
+  DialogPortal,
+  DialogRoot,
+  DialogTitle,
+} from "./primitives";
+import {
+  DialogBody,
+  DialogFooter,
+  DialogHeader,
+  DialogSection,
+  FieldStack,
+  FormRow,
+} from "./sections";
+export {
+  DialogSection,
+  DIALOG_OVERLAY_CLASS_NAME,
+  DialogTitle,
+  DialogContent,
+  DialogRoot,
+  DialogHeader,
+  DialogLayout,
+  DialogPortal,
+  DialogBody,
+  FieldStack,
+  DialogOverlay,
+  DialogFooter,
+  FormRow,
+  DIALOG_FOOTER_BUTTON_CLASS,
+  DialogDescription,
+};
