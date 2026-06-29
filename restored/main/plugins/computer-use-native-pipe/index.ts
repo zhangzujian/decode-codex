ab676e7cfd3a499b66050c3b90b1a17f1f35e616
Jim Liu 宝玉 <junminliu@gmail.com>
2026-06-29 12:03:58 -0500

    main: restore computer use native pipe helpers

---
+++ restored/main/plugins/computer-use-native-pipe/index.ts
@@ -0,0 +1,11 @@
+// Restored from ref/.vite/build/main-r5HnecX_.js
+// Barrel for Computer Use native pipe helpers.
+
+export * from "./analytics";
+export * from "./approval-bridge";
+export * from "./controller";
+export * from "./env";
+export * from "./framing";
+export * from "./notify-config";
+export * from "./server";
+export * from "./types";
