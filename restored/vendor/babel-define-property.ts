b4faef35bf4dbb9fe5a9c467e31718524c199177
Jim Liu 宝玉 <junminliu@gmail.com>
2026-06-30 08:30:54 -0500

    restore babel define property helper

--- restored/vendor/babel-define-property.ts
+++ restored/vendor/babel-define-property.ts
@@ -1,18 +1,15 @@
 // Restored from ref/webview/assets/_defineProperty-CjRaK4yh.js
 // DefineProperty chunk restored from the Codex webview bundle.
 import { createCommonJsModule } from "../runtime/commonjs-interop";
-import { isEqualW } from "./lodash-is-equal";
-export const defineProperty = createCommonJsModule(
-  (definePropertyParam1, _defineProperty) => {
-    var definePropertyValue1 = isEqualW();
-    _defineProperty.exports = (function () {
-      try {
-        var definePropertyValue2 = definePropertyValue1(
-          Object,
-          "defineProperty",
-        );
-        return (definePropertyValue2({}, "", {}), definePropertyValue2);
-      } catch {}
-    })();
-  },
-);
+import { isEqualW as createGetNativeProperty } from "./lodash-is-equal";
+
+export const defineProperty = createCommonJsModule((_exports, module) => {
+  const getNativeProperty = createGetNativeProperty();
+  module.exports = (() => {
+    try {
+      const objectDefineProperty = getNativeProperty(Object, "defineProperty");
+      objectDefineProperty({}, "", {});
+      return objectDefineProperty;
+    } catch {}
+  })();
+});
