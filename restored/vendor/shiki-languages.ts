6cf565e935956df21c669ced03cc573b9030501f
Jim Liu 宝玉 <junminliu@gmail.com>
2026-06-30 06:48:58 -0500

    restore additional shiki language aliases

--- restored/vendor/shiki-languages.ts
+++ restored/vendor/shiki-languages.ts
@@ -41,6 +41,46 @@ import genieGrammar from "@shikijs/langs/genie";
 import gherkinGrammar from "@shikijs/langs/gherkin";
 import gleamGrammar from "@shikijs/langs/gleam";
 import gnuplotGrammar from "@shikijs/langs/gnuplot";
+import groovyGrammar from "@shikijs/langs/groovy";
+import haskellGrammar from "@shikijs/langs/haskell";
+import hclGrammar from "@shikijs/langs/hcl";
+import hjsonGrammar from "@shikijs/langs/hjson";
+import hyGrammar from "@shikijs/langs/hy";
+import imbaGrammar from "@shikijs/langs/imba";
+import iniGrammar from "@shikijs/langs/ini";
+import json5Grammar from "@shikijs/langs/json5";
+import jsoncGrammar from "@shikijs/langs/jsonc";
+import jsonlGrammar from "@shikijs/langs/jsonl";
+import jsonnetGrammar from "@shikijs/langs/jsonnet";
+import jssmGrammar from "@shikijs/langs/jssm";
+import kdlGrammar from "@shikijs/langs/kdl";
+import kotlinGrammar from "@shikijs/langs/kotlin";
+import kustoGrammar from "@shikijs/langs/kusto";
+import leanGrammar from "@shikijs/langs/lean";
+import llvmGrammar from "@shikijs/langs/llvm";
+import logGrammar from "@shikijs/langs/log";
+import logoGrammar from "@shikijs/langs/logo";
+import luauGrammar from "@shikijs/langs/luau";
+import makeGrammar from "@shikijs/langs/make";
+import matlabGrammar from "@shikijs/langs/matlab";
+import mdxGrammar from "@shikijs/langs/mdx";
+import mipsasmGrammar from "@shikijs/langs/mipsasm";
+import mojoGrammar from "@shikijs/langs/mojo";
+import moveGrammar from "@shikijs/langs/move";
+import narratGrammar from "@shikijs/langs/narrat";
+import nextflowGrammar from "@shikijs/langs/nextflow";
+import nushellGrammar from "@shikijs/langs/nushell";
+import objectiveCGrammar from "@shikijs/langs/objective-c";
+import objectiveCppGrammar from "@shikijs/langs/objective-cpp";
+import ocamlGrammar from "@shikijs/langs/ocaml";
+import openscadGrammar from "@shikijs/langs/openscad";
+import pascalGrammar from "@shikijs/langs/pascal";
+import pklGrammar from "@shikijs/langs/pkl";
+import plsqlGrammar from "@shikijs/langs/plsql";
+import poGrammar from "@shikijs/langs/po";
+import polarGrammar from "@shikijs/langs/polar";
+import powerqueryGrammar from "@shikijs/langs/powerquery";
+import powershellGrammar from "@shikijs/langs/powershell";
 
 export {
   abapGrammar,
@@ -83,4 +123,44 @@ export {
   gherkinGrammar,
   gleamGrammar,
   gnuplotGrammar,
+  groovyGrammar,
+  haskellGrammar,
+  hclGrammar,
+  hjsonGrammar,
+  hyGrammar,
+  imbaGrammar,
+  iniGrammar,
+  json5Grammar,
+  jsoncGrammar,
+  jsonlGrammar,
+  jsonnetGrammar,
+  jssmGrammar,
+  kdlGrammar,
+  kotlinGrammar,
+  kustoGrammar,
+  leanGrammar,
+  llvmGrammar,
+  logGrammar,
+  logoGrammar,
+  luauGrammar,
+  makeGrammar,
+  matlabGrammar,
+  mdxGrammar,
+  mipsasmGrammar,
+  mojoGrammar,
+  moveGrammar,
+  narratGrammar,
+  nextflowGrammar,
+  nushellGrammar,
+  objectiveCGrammar,
+  objectiveCppGrammar,
+  ocamlGrammar,
+  openscadGrammar,
+  pascalGrammar,
+  pklGrammar,
+  plsqlGrammar,
+  poGrammar,
+  polarGrammar,
+  powerqueryGrammar,
+  powershellGrammar,
 };
