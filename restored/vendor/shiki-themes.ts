aea5511703bcb8e5166184d6e136398777da76f0
Jim Liu 宝玉 <junminliu@gmail.com>
2026-06-30 06:04:45 -0500

    restore shiki theme vendor aliases

---
+++ restored/vendor/shiki-themes.ts
@@ -0,0 +1,22 @@
+// Restored from ref/webview/assets/ayu-dark-C-RFidiT.js
+// Also covers current Shiki theme chunks in ref/webview/assets.
+// Shiki bundled theme data is kept as npm leaf imports instead of copying JSON dumps.
+import ayuDarkTheme from "@shikijs/themes/ayu-dark";
+import catppuccinLatteTheme from "@shikijs/themes/catppuccin-latte";
+import catppuccinMochaTheme from "@shikijs/themes/catppuccin-mocha";
+import darkPlusTheme from "@shikijs/themes/dark-plus";
+import draculaTheme from "@shikijs/themes/dracula";
+import everforestDarkTheme from "@shikijs/themes/everforest-dark";
+import githubDarkDefaultTheme from "@shikijs/themes/github-dark-default";
+import githubLightDefaultTheme from "@shikijs/themes/github-light-default";
+
+export {
+  ayuDarkTheme,
+  catppuccinLatteTheme,
+  catppuccinMochaTheme,
+  darkPlusTheme,
+  draculaTheme,
+  everforestDarkTheme,
+  githubDarkDefaultTheme,
+  githubLightDefaultTheme,
+};
