0295d6e6a9a705274d5fcb52251faacd31b43acc
Jim Liu 宝玉 <junminliu@gmail.com>
2026-06-30 08:54:47 -0500

    finalize rolldown runtime interop

--- restored/runtime/commonjs-interop.ts
+++ restored/runtime/commonjs-interop.ts
@@ -1,11 +1,15 @@
 // Restored from ref/webview/assets/rolldown-runtime-Czos8NxU.js
-// Also matches ref/webview/assets/rolldown-runtime-Czos8NxU.js.
 // Rolldown CommonJS/ESM interop helpers restored from the Codex webview bundle.
-type AnyModule = Record<PropertyKey, any>;
-type CommonJsCallback = (
-  exports: AnyModule,
-  module: { exports: AnyModule },
+type MutableModule = Record<PropertyKey, unknown>;
+type GetterMap = Record<string, () => unknown>;
+type RuntimeRequire = (moduleName: string, ...args: unknown[]) => unknown;
+type CommonJsModule<TExports = MutableModule> = { exports: TExports };
+type CommonJsCallback<TExports = MutableModule> = (
+  exports: MutableModule,
+  module: CommonJsModule<TExports>,
 ) => void;
+
+declare const require: RuntimeRequire | undefined;
 
 const objectCreate = Object.create;
 const defineProperty = Object.defineProperty;
@@ -14,46 +18,58 @@ const getOwnPropertyNames = Object.getOwnPropertyNames;
 const getPrototypeOf = Object.getPrototypeOf;
 const hasOwnProperty = Object.prototype.hasOwnProperty;
 
-export const commonJsRequire = ((fallbackRequire: any) =>
-  typeof require < "u"
-    ? require
-    : typeof Proxy < "u"
-      ? new Proxy(fallbackRequire, {
-          get: (target, property) =>
-            (typeof require < "u" ? require : target)[property],
-        })
-      : fallbackRequire)(function requireUnavailable(
-  moduleName: string,
-): never {
-  if (typeof require < "u") return require.apply(this, arguments as any);
-  throw Error(
+function getRuntimeRequire(): RuntimeRequire | undefined {
+  return typeof require !== "undefined" ? require : undefined;
+}
+
+function requireUnavailable(moduleName: string): unknown {
+  const runtimeRequire = getRuntimeRequire();
+  if (runtimeRequire) return runtimeRequire(moduleName);
+
+  throw new Error(
     'Calling `require` for "' +
       moduleName +
       "\" in an environment that doesn't expose the `require` function. See https://rolldown.rs/in-depth/bundling-cjs#require-external-modules for more details.",
   );
-});
+}
+
+export const commonJsRequire: RuntimeRequire = ((fallbackRequire) => {
+  const runtimeRequire = getRuntimeRequire();
+  if (runtimeRequire) return runtimeRequire;
+
+  return typeof Proxy !== "undefined"
+    ? new Proxy(fallbackRequire, {
+        get: (target, property) =>
+          (getRuntimeRequire() ?? target)[property as keyof RuntimeRequire],
+      })
+    : fallbackRequire;
+})(requireUnavailable);
 
 export const copyCommonJsProperties = (
-  target: AnyModule,
-  source: AnyModule,
-  secondTarget?: AnyModule,
-): void => {
+  target: MutableModule,
+  source: unknown,
+  secondTarget?: MutableModule,
+): MutableModule | undefined => {
   copyProperties(target, source, "default");
-  if (secondTarget) copyProperties(secondTarget, source, "default");
+  return secondTarget == null
+    ? undefined
+    : copyProperties(secondTarget, source, "default");
 };
 
 export const once =
   <T>(initializer: ((value: 0) => T) | undefined, value?: T) =>
   () => {
     if (initializer) {
-      value = initializer((initializer = undefined as never));
+      const runInitializer = initializer;
+      initializer = undefined;
+      value = runInitializer(0);
     }
     return value;
   };
 
-export const toCommonJs = (module: AnyModule): AnyModule =>
+export const toCommonJs = (module: MutableModule): MutableModule =>
   hasOwnProperty.call(module, "module.exports")
-    ? module["module.exports"]
+    ? (module["module.exports"] as MutableModule)
     : copyProperties(
         defineProperty({}, "__esModule", {
           value: true,
@@ -62,10 +78,10 @@ export const toCommonJs = (module: AnyModule): AnyModule =>
       );
 
 export const exportGetters = (
-  getters: Record<string, () => unknown>,
+  getters: GetterMap,
   tagAsModule?: boolean,
-): AnyModule => {
-  const result: AnyModule = {};
+): MutableModule => {
+  const result: MutableModule = {};
   for (const key in getters) {
     defineProperty(result, key, {
       get: getters[key],
@@ -80,47 +96,52 @@ export const exportGetters = (
   return result;
 };
 
-export const toEsModule = (
-  module: AnyModule | null | undefined,
-  target?: AnyModule,
-): AnyModule => {
+export const toEsModule = <TValue>(
+  module: TValue,
+  forceDefaultExport?: unknown,
+): MutableModule & { default?: TValue } => {
   const result = module == null ? {} : objectCreate(getPrototypeOf(module));
   return copyProperties(
-    target || !module || !module.__esModule
+    forceDefaultExport ||
+      !module ||
+      !(module as { __esModule?: boolean }).__esModule
       ? defineProperty(result, "default", {
           value: module,
           enumerable: true,
         })
       : result,
     module,
-  );
+  ) as MutableModule & { default?: TValue };
 };
 
 export const createCommonJsModule =
-  (callback: CommonJsCallback, cache?: { exports: AnyModule }) => () => {
+  <TExports = MutableModule>(
+    callback: CommonJsCallback<TExports>,
+    cache?: CommonJsModule<TExports>,
+  ) =>
+  (): TExports => {
     if (!cache) {
       cache = {
-        exports: {},
+        exports: {} as TExports,
       };
-      callback(cache.exports, cache);
+      callback(cache.exports as MutableModule, cache);
     }
     return cache.exports;
   };
 
 function copyProperties(
-  target: AnyModule,
-  source: AnyModule | null | undefined,
+  target: MutableModule,
+  source: unknown,
   except?: PropertyKey,
-  descriptor?: PropertyDescriptor,
-): AnyModule {
+): MutableModule {
   if ((source && typeof source === "object") || typeof source === "function") {
     for (const key of getOwnPropertyNames(source)) {
       if (!hasOwnProperty.call(target, key) && key !== except) {
+        const propertyKey = key;
+        const descriptor = getOwnPropertyDescriptor(source, key);
         defineProperty(target, key, {
-          get: () => source[key],
-          enumerable:
-            !(descriptor = getOwnPropertyDescriptor(source, key)) ||
-            descriptor.enumerable,
+          get: () => (source as MutableModule)[propertyKey],
+          enumerable: descriptor == null || descriptor.enumerable,
         });
       }
     }
