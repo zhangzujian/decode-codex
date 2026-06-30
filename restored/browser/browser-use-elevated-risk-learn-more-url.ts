6e9640611cfc265656033e6876d74fcc0d2122dc
Jim Liu 宝玉 <junminliu@gmail.com>
2026-06-30 12:29:56 -0500

    restore browser use elevated risk url current hash

--- restored/browser/browser-use-elevated-risk-learn-more-url.ts
+++ restored/browser/browser-use-elevated-risk-learn-more-url.ts
@@ -1,4 +1,5 @@
-// Restored from ref/webview/assets/browser-use-elevated-risk-learn-more-url-SLRkBp9s.js
+// Restored from ref/webview/assets/browser-use-elevated-risk-learn-more-url-Bdqa5sCw.js
+// browser-use-elevated-risk-learn-more-url-Bdqa5sCw chunk restored from the Codex webview bundle.
 import { CODEX_CHROME_EXTENSION_DOCS_URL } from "../utils/links-bd-mmkun-d";
 import { getStatsigUrlConfig } from "../utils/statsig-url-config";
 const BROWSER_USE_ELEVATED_RISK_LEARN_MORE_CONFIG_ID = "4168530037";
