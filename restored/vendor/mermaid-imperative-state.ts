df615437b91e681d6df7a6b9fe0662a26a913fa8
Jim Liu 宝玉 <junminliu@gmail.com>
2026-06-30 08:40:22 -0500

    restore mermaid imperative state helper

--- restored/vendor/mermaid-imperative-state.ts
+++ restored/vendor/mermaid-imperative-state.ts
@@ -1,15 +1,21 @@
 // Restored from ref/webview/assets/chunk-QZHKN3VN-C6aDqNWX.js
 // ChunkQZHKN3VN chunk restored from the Codex webview bundle.
 import { chunkAGHRB4JFN } from "./dompurify";
-export const ChunkQZHKN3VN = class {
-  constructor(chunkQZHKN3VNParam1) {
-    this.init = chunkQZHKN3VNParam1;
+
+class ImperativeState<TRecords = unknown> {
+  public records: TRecords;
+
+  public constructor(public readonly init: () => TRecords) {
     this.records = this.init();
   }
+
   static {
     chunkAGHRB4JFN(this, "ImperativeState");
   }
-  reset() {
+
+  public reset(): void {
     this.records = this.init();
   }
-};
+}
+
+export { ImperativeState, ImperativeState as ChunkQZHKN3VN };
