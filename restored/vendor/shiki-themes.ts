2ab719bc513cd7160e117488efe1428e51ecfc80
Jim Liu 宝玉 <junminliu@gmail.com>
2026-06-30 06:12:42 -0500

    restore additional shiki theme aliases

--- restored/vendor/shiki-themes.ts
+++ restored/vendor/shiki-themes.ts
@@ -1,37 +1,65 @@
 // Restored from ref/webview/assets/ayu-dark-C-RFidiT.js
 // Also covers current Shiki theme chunks in ref/webview/assets.
 // Shiki bundled theme data is kept as npm leaf imports instead of copying JSON dumps.
+import andromeedaTheme from "@shikijs/themes/andromeeda";
+import auroraXTheme from "@shikijs/themes/aurora-x";
 import ayuDarkTheme from "@shikijs/themes/ayu-dark";
+import ayuLightTheme from "@shikijs/themes/ayu-light";
+import ayuMirageTheme from "@shikijs/themes/ayu-mirage";
+import catppuccinFrappeTheme from "@shikijs/themes/catppuccin-frappe";
 import catppuccinLatteTheme from "@shikijs/themes/catppuccin-latte";
+import catppuccinMacchiatoTheme from "@shikijs/themes/catppuccin-macchiato";
 import catppuccinMochaTheme from "@shikijs/themes/catppuccin-mocha";
 import darkPlusTheme from "@shikijs/themes/dark-plus";
+import draculaSoftTheme from "@shikijs/themes/dracula-soft";
 import draculaTheme from "@shikijs/themes/dracula";
 import everforestDarkTheme from "@shikijs/themes/everforest-dark";
 import everforestLightTheme from "@shikijs/themes/everforest-light";
+import githubDarkDimmedTheme from "@shikijs/themes/github-dark-dimmed";
+import githubDarkHighContrastTheme from "@shikijs/themes/github-dark-high-contrast";
 import githubDarkTheme from "@shikijs/themes/github-dark";
 import githubDarkDefaultTheme from "@shikijs/themes/github-dark-default";
+import githubLightHighContrastTheme from "@shikijs/themes/github-light-high-contrast";
 import githubLightTheme from "@shikijs/themes/github-light";
 import githubLightDefaultTheme from "@shikijs/themes/github-light-default";
+import gruvboxDarkHardTheme from "@shikijs/themes/gruvbox-dark-hard";
 import gruvboxDarkMediumTheme from "@shikijs/themes/gruvbox-dark-medium";
+import gruvboxDarkSoftTheme from "@shikijs/themes/gruvbox-dark-soft";
+import gruvboxLightHardTheme from "@shikijs/themes/gruvbox-light-hard";
 import gruvboxLightMediumTheme from "@shikijs/themes/gruvbox-light-medium";
+import gruvboxLightSoftTheme from "@shikijs/themes/gruvbox-light-soft";
 import lightPlusTheme from "@shikijs/themes/light-plus";
 import materialThemeDarkerTheme from "@shikijs/themes/material-theme-darker";
 import monokaiTheme from "@shikijs/themes/monokai";
 
 export {
+  andromeedaTheme,
+  auroraXTheme,
   ayuDarkTheme,
+  ayuLightTheme,
+  ayuMirageTheme,
+  catppuccinFrappeTheme,
   catppuccinLatteTheme,
+  catppuccinMacchiatoTheme,
   catppuccinMochaTheme,
   darkPlusTheme,
+  draculaSoftTheme,
   draculaTheme,
   everforestDarkTheme,
   everforestLightTheme,
+  githubDarkDimmedTheme,
+  githubDarkHighContrastTheme,
   githubDarkTheme,
   githubDarkDefaultTheme,
+  githubLightHighContrastTheme,
   githubLightTheme,
   githubLightDefaultTheme,
+  gruvboxDarkHardTheme,
   gruvboxDarkMediumTheme,
+  gruvboxDarkSoftTheme,
+  gruvboxLightHardTheme,
   gruvboxLightMediumTheme,
+  gruvboxLightSoftTheme,
   lightPlusTheme,
   materialThemeDarkerTheme,
   monokaiTheme,
