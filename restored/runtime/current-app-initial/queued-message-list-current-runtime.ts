5588fdaf6e8f6ec4ca7785c9d5a36e674327df15
Jim Liu 宝玉 <junminliu@gmail.com>
2026-06-30 20:49:01 -0500

    restore queued message list surface

--- restored/runtime/current-app-initial/queued-message-list-current-runtime.ts
+++ restored/runtime/current-app-initial/queued-message-list-current-runtime.ts
@@ -1,3 +1,3 @@
 // Restored from ref/webview/assets/queued-message-list-B8Ie-nMz.js
 // Current-ref compatibility export surface for QueuedMessageList.
-export { QueuedMessageList } from "../../vendor/queued-message-list-current-bundle";
+export { QueuedMessageList } from "../../composer/queued-message-list";
