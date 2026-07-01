ff9461726e8a39b2b2403a21185ecef0febfaec3
Jim Liu 宝玉 <junminliu@gmail.com>
2026-06-30 21:13:47 -0500

    restore current git config query surface

--- restored/runtime/current-app-initial/git-config-value-current-runtime.ts
+++ restored/runtime/current-app-initial/git-config-value-current-runtime.ts
@@ -1,7 +1,9 @@
 // Restored from ref/webview/assets/app-initial~app-main~remote-conversation-page~pull-requests-page~new-thread-panel-page~proj~jolrh1c9-UBdbdM9i.js
 // Complete current-ref compatibility export surface for gitConfigValueCurrent.
-// prettier-ignore
 export {
-  initGitConfigValueCurrentRuntimeDependencies as gitConfigValueCurrentCompatSlotLowerA, createCurrentGitQuerySignal as gitConfigValueCurrentCompatSlotLowerI, gitConfigValueFromCwdSignal as gitConfigValueCurrentCompatSlotLowerN, initGitConfigValueCurrentChunk as gitConfigValueCurrentCompatSlotLowerR,
-  gitConfigValueByMetadataQuerySignal as gitConfigValueCurrentCompatSlotLowerT
-} from "../../vendor/git-config-value-current-bundle";
+  initGitConfigValueCurrentRuntimeDependencies as gitConfigValueCurrentCompatSlotLowerA,
+  createCurrentGitQuerySignal as gitConfigValueCurrentCompatSlotLowerI,
+  gitConfigValueFromCwdSignal as gitConfigValueCurrentCompatSlotLowerN,
+  initGitConfigValueCurrentChunk as gitConfigValueCurrentCompatSlotLowerR,
+  gitConfigValueByMetadataQuerySignal as gitConfigValueCurrentCompatSlotLowerT,
+} from "../../github/git-config-value-current";
